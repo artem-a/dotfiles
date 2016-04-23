26c9b0f7fd60104d20ce5cfcf7e6f56640c6d930
Artem A <artem.v.andreev@gmail.com>
2016-04-23 12:38:28 +0300

    add fish config

---
+++ dotfiles/config.fish
@@ -0,0 +1,8 @@
+set -x TERM xterm-256color
+set -U EDITOR vim
+
+set PATH $HOME/bin $PATH
+set PATH $HOME/.rbenv/bin $PATH
+set PATH $HOME/.rbenv/plugins/ruby-build/bin $PATH
+
+status --is-interactive; and . (rbenv init - fish|psub)

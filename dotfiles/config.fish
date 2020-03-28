3e6f53dd4804199bc8d3bcfed603835c29d107b3
Artem A <artem.v.andreev@gmail.com>
2020-03-28 12:11:43 +0200

    add GOPATH into config.fish

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,6 +1,8 @@
 set -x TERM xterm-256color
 set -U EDITOR vim
 
+set -x GOPATH $HOME/gocode
+
 set PATH $HOME/bin $PATH
 set PATH $HOME/.rbenv/bin $PATH
 set PATH $HOME/.rbenv/plugins/ruby-build/bin $PATH
